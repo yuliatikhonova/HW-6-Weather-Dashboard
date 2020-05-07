c2c0be8d4d5b5c5dab513b592759d5399d61a81d
yulia tikhonova <yuliatikhonova@outlook.com>
2020-05-07 01:36:41 -0700

    finished the current weather data pull

--- Assets/script.js
+++ Assets/script.js
@@ -1,35 +1,42 @@
 //------------------Variables------------------------
 
-var APIKey = 'be2657f4805264720d4f2be16afc80a4';
-var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=' + APIKey
+var APIKey = 'be2657f4805264720d4f2be16afc80a4';//Tested key. Still working
+var queryCurrentURL = 'https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=' + APIKey;//url to pull data from
 
-var cityInput = $('#cityInput');
+// var cityInput = $("#cityInput").val();
+
 var searchBtn = $('#searchBtn');
 
 //-------------------Calling to the OpenWeatherMap API ------------------
 
+
+
 $(searchBtn).on('click', function () {
-    cityInput = $('#cityInput').val();
-    $.ajax({
-        url: queryURL,
+    $.ajax({//Get request for current temperature
+        url: queryCurrentURL,
         method: "GET"
-    })
-        .then(function (response) {
-            console.log(queryURL);
-            console.log(response);
+    }).then(function (response) {
+        console.log(response)
 
+        $("#city").html("<h1>" + response.name + "</h1>");
+        $("#wind").text("Wind Speed: " + response.wind.speed);
+        $("#humidity").text("Humidity: " + response.main.humidity);
 
-            $("#city").html("<h1>" + response.name + " Weather Details</h1>"); 
-                $("#wind").text("Wind Speed: " + response.wind.speed);
-                $("#humidity").text("Humidity: " + response.main.humidity);
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $("#temp").text("Temperature: " + tempF.toFixed(2) + ' F');
 
-            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-            $("#temp").text("Temperature (F) " + tempF.toFixed(2));
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + tempF);
+    });
 
-            console.log("Wind Speed: " + response.wind.speed);
-            console.log("Humidity: " + response.main.humidity);
-            console.log("Temperature (F): " + tempF);
-
-        });
+    //request for uv data
+    
+    //request for 5day forecast
 
 });
+
+
+
+
+
