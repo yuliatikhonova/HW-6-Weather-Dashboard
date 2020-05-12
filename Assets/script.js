0ad0a67fb0b149e5883f26708843942042a719c1
yulia tikhonova <yuliatikhonova@outlook.com>
2020-05-12 13:31:12 -0700

    added the uv index to the page

--- Assets/script.js
+++ Assets/script.js
@@ -2,14 +2,17 @@
 //-------------------Calling to the OpenWeatherMap API ------------------
 
 var searchBtn = $('#searchBtn');
+var lon = "";
 
 $(searchBtn).on('click', function () {
     var cityInput = $("#cityInput").val();
 
     var APIKey = 'be2657f4805264720d4f2be16afc80a4';//Tested key. Still working
-    var queryCurrentURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=' + APIKey;//url to pull data from
-    var queryUVUrl = '';
-    var queryForecastURL = '';
+    var queryCurrentURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=' + APIKey;//url to pull current weather data
+
+
+
+    //var queryForecastURL = '';
 
     // Request and response for current temperature, humidity and wind speed.
     $.ajax({
@@ -17,25 +20,36 @@ $(searchBtn).on('click', function () {
         method: "GET"
     }).then(function (response) {
         console.log(response)
-        var response = response;
         $("#city").html("<h1>" + response.name + ' (' + moment().format('M/D/YYYY') + ')' + "<img src='http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'>" + "</h1>");
         $("#wind").text("Wind Speed: " + response.wind.speed);
         $("#humidity").text("Humidity: " + response.main.humidity);
 
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        $("#temp").text("Temperature: " + tempF.toFixed(2) + ' F');
+        $("#temp").text("Temperature: " + tempF.toFixed(2) + ' F');       
 
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+        console.log(lon);
+        console.log(lat);
+        var queryUVUrl = 'http://api.openweathermap.org/data/2.5/uvi?appid=' + APIKey  + '&lat=' + lat  + '&lon=' + lon;
+        
+        // Request and response for uv data
+        $.ajax({
+            url: queryUVUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.value);
+            $("#uv").text("UV Index: " + response.value);
+        });
     });
 
-    // Request and response for uv data
+
+
+
 
     // Request and response for 5day forecast
 
 });
-
 
 
 
