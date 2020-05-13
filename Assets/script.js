984d02b6601a7c9072e1b74b6e52272f83017ebb
yulia tikhonova <yuliatikhonova@outlook.com>
2020-05-13 14:52:44 -0700

    got the 5 day forecast to work

--- Assets/script.js
+++ Assets/script.js
@@ -43,20 +43,50 @@ $(searchBtn).on('click', function () {
         });
     });
 
-    $('#forecast').prepend('<h1>5-Day Forecast:</h1>');
-
     // Request and response for 5day forecast
     $.ajax({
         url: queryForecastURL,
             method: "GET"
     }).then(function(response) {
-        for (var i = 2; i < response.list.length; i+=8) {  
-            console.log(response.list[i]);
-            {$('#date').append('<h2>' + response.list[i].dt_txt + '</h2>');
-            $('#icon').append("<img src='http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png'>");
-            $('#tempe').append('Temp: ' + response.list[i].main.temp + " F");
-            $('#humid').append('Humidity: ' + response.list[i].main.humidity + ' %');}
-        };
+        //$('#forecast').text('<h1>5-Day Forecast:</h1>');
+
+        var day1 = $('#day1');
+        day1.html('<h2>' + response.list[6].dt_txt + '</h2>');
+        day1.append("<img src='http://openweathermap.org/img/wn/" + response.list[6].weather[0].icon + "@2x.png'>");
+        day1.append('<p>Temp: ' + response.list[6].main.temp + " F</p>");
+        day1.append('<p>Humidity: ' + response.list[6].main.humidity + ' %</p>');
+
+        var day2 = $('#day2');
+        day2.html('<h2>' + response.list[14].dt_txt + '</h2>');
+        day2.append("<img src='http://openweathermap.org/img/wn/" + response.list[14].weather[0].icon + "@2x.png'>");
+        day2.append('<p>Temp: ' + response.list[14].main.temp + " F</p>");
+        day2.append('<p>Humidity: ' + response.list[14].main.humidity + ' %</p>');
+
+        var day3 = $('#day3');
+        day3.html('<h2>' + response.list[22].dt_txt + '</h2>');
+        day3.append("<img src='http://openweathermap.org/img/wn/" + response.list[22].weather[0].icon + "@2x.png'>");
+        day3.append('<p>Temp: ' + response.list[22].main.temp + " F</p>");
+        day3.append('<p>Humidity: ' + response.list[22].main.humidity + ' %</p>');
+
+        var day4 = $('#day4');
+        day4.html('<h2>' + response.list[30].dt_txt + '</h2>');
+        day4.append("<img src='http://openweathermap.org/img/wn/" + response.list[30].weather[0].icon + "@2x.png'>");
+        day4.append('<p>Temp: ' + response.list[30].main.temp + " F</p>");
+        day4.append('<p>Humidity: ' + response.list[30].main.humidity + ' %</p>');
+
+        var day5 = $('#day5');
+        day5.html('<h2>' + response.list[38].dt_txt + '</h2>');
+        day5.append("<img src='http://openweathermap.org/img/wn/" + response.list[38].weather[0].icon + "@2x.png'>");
+        day5.append('<p>Temp: ' + response.list[38].main.temp + " F</p>");
+        day5.append('<p>Humidity: ' + response.list[38].main.humidity + ' %</p>');
+
+        // for (var i = 6; i < response.list.length; i+=8) {  
+        //     console.log(response.list[i]);
+        //     $('#date').append('<h2>' + response.list[i].dt_txt + '</h2>');
+        //     $('#icon').append("<img src='http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png'>");
+        //     $('#tempe').append('Temp: ' + response.list[i].main.temp + " F");
+        //     $('#humid').append('Humidity: ' + response.list[i].main.humidity + ' %');
+        // };
     });
 
 
