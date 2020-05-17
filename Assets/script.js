b39869c19acc52f81ead92c741e096327000c0ac
yulia tikhonova <yuliatikhonova@outlook.com>
2020-05-17 01:41:01 -0700

    was able to rendor the data and switch between citire

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,24 @@
-var cities = ["Phoenix", "Flagstaff", "Tucson", "Plano"];
+var cities = [""];
 
-function displayWeatherInfo() {
-
+function displayWeatherInfo(city) {
     var APIKey = 'be2657f4805264720d4f2be16afc80a4';//Tested key. Still working
 
-    var weather = $(this).attr("data-name");
-    
-
-    var queryCurrentURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + weather + '&units=imperial&appid=' + APIKey;//url to pull current weather data
-    var queryForecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + weather + '&units=imperial&appid=' + APIKey;//url to pull forecast weather data
+    var queryCurrentURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + APIKey;//url to pull current weather data
+    var queryForecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + APIKey;//url to pull forecast weather data
 
     //Request and response for current temperature, humidity and wind speed.
     $.ajax({
         url: queryCurrentURL,
         method: "GET"
     }).then(function (response) {
-
-        var weatherDiv = $("<div class='weather'>");
-
-        $("#city").html("<h1>" + response.name + ' (' + moment().format('M/D/YYYY') + ')' + "<img src='http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'>" + "</h1>");
-        $("#wind").append("<p>Wind Speed: " + response.wind.speed + "</p>");
-        $("#humidity").append("<p>Humidity: " + response.main.humidity + "</p>");
-        $("#temp").append("<p>Temperature: " + response.main.temp + ' F</p>');
+        var dataDiv = $("#data");
+        dataDiv.html("<h1>" + response.name + ' (' + moment().format('M/D/YYYY') + ')' + "<img src='http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'>" + "</h1>");
+        dataDiv.append("<p>Wind Speed: " + response.wind.speed + "</p>");
+        dataDiv.append("<p>Humidity: " + response.main.humidity + "</p>");
+        dataDiv.append("<p>Temperature: " + response.main.temp + ' F</p>');
 
         var lon = response.coord.lon;
         var lat = response.coord.lat;
-
         var queryUVUrl = 'https://api.openweathermap.org/data/2.5/uvi?appid=' + APIKey + '&lat=' + lat + '&lon=' + lon;
 
         // Request and response for uv index data
@@ -33,9 +26,7 @@ function displayWeatherInfo() {
             url: queryUVUrl,
             method: "GET"
         }).then(function (response) {
-
-            $("#uv").append("<p>UV Index: " + response.value + "</p>");
-
+            dataDiv.append("<p>UV Index: " + response.value + "</p>");
         });
         
         // Request and response for 5day forecast
@@ -43,36 +34,38 @@ function displayWeatherInfo() {
             url: queryForecastURL,
             method: "GET"
         }).then(function (response) {
-
             for (var i = 6; i < response.list.length; i += 8) {
 
-                var day = $('#day');
+                var day = $("#day");
                 day.append('<h4>' + response.list[i].dt_txt + '</h4>');
                 day.append("<img src='http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png'>");
                 day.append('<p>Temp: ' + response.list[i].main.temp + " F</p>");
                 day.append('<p>Humidity: ' + response.list[i].main.humidity + ' %</p>');
+
+
             };
         });
 
-        // Putting the entire movie above the previous movies
-        $("#allData").prepend(weatherDiv);//adjust to replace the new weather
 
     });
+
+
 };
 
 
-
+//Function for the render button
 function renderButtons(city) {
+    //$(".citiesDump").empty();
 
-       var btn = $("<button>");
-        
-        btn.addClass("weather-btn");
-        
-        btn.attr("data-name", city);
-        
-        btn.text(city);
-        
-        $(".cities-array").append(btn);
+    var btn = $("<button>");//Dynamically adding the button
+
+    btn.addClass("city-btn");//Adding a class to the button
+
+    btn.attr("data-name", city);//Adding a data-attribute
+
+    btn.text(city);//Adding text to the button
+
+    $(".citiesDump").append(btn);//Inserts the button to the page
 
 };
 
@@ -82,13 +75,18 @@ var searchBtn = $('#searchBtn');
 
 searchBtn.on("click", function (event) {//the on click function
     event.preventDefault();//So the form does not refresh the page
-    
-    var theWeather = $("#cityInput").val();//Get the input value from the user
 
-    cities.push(theWeather);//Push the value, aka city, into the array.
+    var city = $("#cityInput").val();//Get the input value from the user
 
-    renderButtons(theWeather);//Execute the render button function with the value passed in
-    displayWeatherInfo(theWeather);//Execute the display info function with the value passed in
+    cities.push(city);//Push the value, aka city, into the array.
+
+    renderButtons(city);//Execute the render button function with the value passed in
+
+    displayWeatherInfo(city);//Execute the display info function with the value passed in
 });
 
-
+//Function to attach the data to the added city button
+$(document).on("click", ".city-btn", function () {
+    var city = $(this).attr("data-name");
+    displayWeatherInfo(city);
+});
